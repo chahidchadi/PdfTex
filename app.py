1e65afa5bd5a51663b914dc6803ecdf66102c160
chahidchadi <87909770+chahidchadi@users.noreply.github.com>
2024-11-06 15:12:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from PIL import Image
 import Pdf_Sliser
 #import Torch_Main
 #from transformers import AutoProcessor, VisionEncoderDecoderModel
-from pathlib import Path
-import PyPDF2
-import base64
-from PIL import Image
-import io
-from io import BytesIO
-from function import final_code_generator
-from function import models , replace_latex_notation
+#from pathlib import Path
+#import PyPDF2
+#import base64
+#from PIL import Image
+#import io
+#from io import BytesIO
+#from function import final_code_generator
+#from function import models , replace_latex_notation
 
 app = Flask(__name__)
 
