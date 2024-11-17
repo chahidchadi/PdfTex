c4a3e943a73ad8f6088424d51091525163249070
chahidchadi <87909770+chahidchadi@users.noreply.github.com>
2024-11-17 15:22:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return send_file('index.html')
-
-@app.route('/about.html')
-def about():
-    return send_file('about.html')
-
-@app.route('/contact.html')
-def contact():
-    return send_file('contact.html')
-
-@app.route('/donate.html')
-def donate():
-    return send_file('donate.html')
-
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
